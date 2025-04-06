ded8b594e13ed3559578390b2f7346d077ad184f
JazzticeGit <asadtevy94@gmail.com>
2025-04-06 19:37:33 +0700

    melengkapi landing page

--- FRONTEND/index.php
+++ FRONTEND/index.php
@@ -1,11 +1,38 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Landing page</title>
 </head>
 <body>
-    
+    <!-- NAVIGASI BAR -->
+    <nav>
+        
+        <div class="navbg">
+            <!-- GAMBAR NAVIGASI -->
+             <a href="index.php"><img src="" alt="" srcset=""></a>
+             <!-- LINK NAVIGASI -->
+            <div class="navlink">
+                <ul>
+                    <li><a href="http://">Shop</a></li>  <!-- SEMENTARA SEBELUM DROPDoWN LINK -->
+                    <li><a href="http://">Collection</a></li>
+                    <li><a href="http://">About</a></li>
+                    <li><a href="http://">Comtack</a></li>
+                </ul>
+            </div>
+            <!-- SEARCH BAR -->
+            <form action="search.php" method="GET">
+            <input type="text" name="query" placeholder="SEARCH  " required>
+            <button type="submit">Cari</button>
+            </form>
+
+            <!-- ICON LINK -->
+             
+        </div>
+    </nav>
 </body>
 </html>
---
+++ FRONTEND/search.php
@@ -0,0 +1,21 @@
+<?php
+include "koneksi.php";
+$query = $_GET['query'];
+// kemananan sql injeksyen
+$query = $koneksi->real_escape_string($query);
+
+$sql = "SELECT * FROM produk WHERE name LIKE '%$query%'";
+$result = $koneksi->query($sql);
+
+// Tampilkan hasil
+if ($result->num_rows > 0) {
+    echo "<h3>Hasil pencarian:</h3>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<p>" . $row['nama_barang'] . " - " . $row['harga'] . "</p>";
+    }
+} else {
+    echo "Barang tidak ditemukan.";
+}
+
+$koneksi->close();
+?>
