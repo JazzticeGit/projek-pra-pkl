ed70c307bb93eee34f71ac644279ba2e6da357fe
JazzticeGit <asadtevy94@gmail.com>
2025-04-07 19:46:14 +0700

    terusan landing page

--- FRONTEND/index.php
+++ FRONTEND/index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing page</title>
     <link rel="stylesheet" href="../STYLESHEET/index_style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 <body>
     <!-- NAVIGASI BAR -->
@@ -12,7 +15,7 @@
         
         <div class="navbg">
             <!-- GAMBAR NAVIGASI -->
-             <a href="index.php"><img src="" alt="" srcset=""></a>
+             <a href="index.php"><img src="../image/AGESA.png" alt="" srcset=""></a>
 
 
              <!-- LINK NAVIGASI -->
@@ -21,7 +24,7 @@
                     <li><a href="http://">Shop</a></li>  <!-- SEMENTARA SEBELUM DROPDoWN LINK -->
                     <li><a href="http://">Collection</a></li>
                     <li><a href="http://">About</a></li>
-                    <li><a href="http://">Comtack</a></li>
+                    <li><a href="http://">Contack</a></li>
                 </ul>
             </div>
 
@@ -30,8 +33,10 @@
             <div class="searchBar">
                 <form action="search.php" method="GET">
                 <input type="text" name="query" placeholder="   Search  " required>
-                <button type="submit">Cari</button>
+                <i class="fas fa-search"></i>
+                <!-- <button type="submit">Cari</button> -->
                 </form>
+                
             </div>
 
 
@@ -44,6 +49,44 @@
              </div>
         </div>
     </nav>
+
+    <!-- FIRST MAIN LAYOUT -->
+
+    <main class="grid-container">
+        
+    <!-- ========== GRID SATU KIRI ATAS  =========== -->
+    <div class="grid_1">
+    <h1>SEMUA ORANG BERHAK KECE!!</h1>
+    <p>Dari outfit kasual hingga busana elegan...</p>
+    <button>View All</button>
+    </div>
+
+    <!-- ==========  GRID DUA BEST SELLER  ========== -->
+    <div class="grid_2">
+    <h3>#BEST SELLER</h3>
+    <img src="baju1.jpg" alt="Best Seller">
+    <button>Learn More</button>
+    </div>
+
+    <!-- =========  GRID EMPAT NEW ARRIVAL  ========== -->
+    <div class="grid_3">
+    <h3>#NEW ARRIVAL</h3>
+    <img src="topi1.jpg" alt="New Arrival">
+    <button>Learn More</button>
+    </div>
+
+    <!-- ===========  GRID EMPAT KANAN PALING BESAR  ========== -->
+    <div class="grid_4">
+    <h2>#Discount Up To <span>50%</span></h2>
+    <img src="jaket1.jpg" alt="Discount Item">
+    <h4>Oversized Boxy Work Jacket</h4>
+    <p>Tetap hangat dan stylish...</p>
+    <p class="price">Rp. 800.000,00</p>
+    <button>Hot Discount</button>
+    </div>
+
+    </main>
+
 </body>
 <script src="https://kit.fontawesome.com/2de2a0ed8e.js" crossorigin="anonymous"></script>
 </html>
