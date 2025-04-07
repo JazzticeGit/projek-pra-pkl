e33ef8f211383985e0bb7dfac26a4a3a3c884bf0
JazzticeGit <asadtevy94@gmail.com>
2025-04-07 13:29:05 +0700

    menambahkan css

--- FRONTEND/index.php
+++ FRONTEND/index.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing page</title>
+    <link rel="stylesheet" href="index_style.css">
 </head>
 <body>
     <!-- NAVIGASI BAR -->
